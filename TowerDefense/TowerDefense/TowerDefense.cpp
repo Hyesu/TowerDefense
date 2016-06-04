ed1e023ff28fd83e78d88a6dd2af6846bd49168d
Hyesu <hotspam0529@gmail.com>
2016-06-04 21:49:45 +0900

    cube render

---
+++ TowerDefense/TowerDefense/TowerDefense.cpp
@@ -0,0 +1,211 @@
+#include "TowerDefense.h"
+TowerDefense* TowerDefense::_pInstance = nullptr;
+const DWORD TowerDefense::Vertex::FVF = D3DFVF_XYZ | D3DFVF_DIFFUSE;
+
+TowerDefense::TowerDefense() {
+	init();
+}
+VOID TowerDefense::init() {
+	_pD3D = nullptr;
+	_pd3dDevice = nullptr;
+
+	_pVertexBuffer = nullptr;
+	_pIndexBuffer = nullptr;	
+}
+
+HRESULT TowerDefense::InitD3D(HWND hWnd)
+{
+	// Create the D3D object, which is needed to create the D3DDevice.
+	if (NULL == (_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
+		return E_FAIL;
+
+	// Set up the structure used to create the D3DDevice.
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.BackBufferWidth = TD_WINDOW_WIDTH;
+	d3dpp.BackBufferHeight = TD_WINDOW_HEIGHT;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+
+	// check device capability for hardware vertex processing
+	D3DCAPS9 caps;
+	_pD3D->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &caps);
+	int nVertexProcessingType;
+	if (caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT)
+		nVertexProcessingType = D3DCREATE_HARDWARE_VERTEXPROCESSING;
+	else
+		nVertexProcessingType = D3DCREATE_SOFTWARE_VERTEXPROCESSING;
+
+	// Create the Direct3D device.
+	if (FAILED(_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+		nVertexProcessingType,
+		&d3dpp, &_pd3dDevice)))
+	{
+		return E_FAIL;
+	}
+
+	// Device state would normally be set here
+
+	return S_OK;
+}
+VOID TowerDefense::Cleanup()
+{
+	if (_pd3dDevice != nullptr)
+		_pd3dDevice->Release();
+
+	if (_pD3D != nullptr)
+		_pD3D->Release();
+
+	if (_pVertexBuffer != nullptr)
+		_pVertexBuffer->Release();
+
+	if (_pIndexBuffer != nullptr)
+		_pIndexBuffer->Release();
+}
+VOID TowerDefense::Render()
+{
+	if (NULL == _pd3dDevice)
+		return;
+
+	// Clear the backbuffer to a TD_BACKGROUND_COLOR
+	_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, TD_BACKGROUND_COLOR, 1.0f, 0);
+
+	// Begin the scene
+	if (SUCCEEDED(_pd3dDevice->BeginScene()))
+	{
+		// Rendering of scene objects can happen here
+		_pd3dDevice->SetStreamSource(0, _pVertexBuffer, 0, sizeof(Vertex));
+		_pd3dDevice->SetIndices(_pIndexBuffer);
+		_pd3dDevice->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE);
+
+		_pd3dDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, TD_NUM_VERTICES, 0, TD_NUM_INDICES / 3);
+
+		// End the scene
+		_pd3dDevice->EndScene();
+	}
+
+	// Present the backbuffer contents to the display
+	_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+HRESULT TowerDefense::SetUp()
+{
+	if (FAILED(initVertexBuffer())) {
+		MessageBox(0, L"InitVertexBuffer Failed", 0, 0);
+		return E_FAIL;
+	}
+	if (FAILED(initIndexBuffer())) {
+		MessageBox(0, L"InitIndexBuffer Failed", 0, 0);
+		return E_FAIL;
+	}
+
+	if (FAILED(initCamera())) {
+		MessageBox(0, L"InitCamera Failed", 0, 0);
+		return E_FAIL;
+	}
+	return S_OK;
+}
+HRESULT TowerDefense::initVertexBuffer()
+{
+	// create vertex buffer
+	if (FAILED(_pd3dDevice->CreateVertexBuffer(
+		TD_NUM_VERTICES * sizeof(Vertex),
+		D3DUSAGE_WRITEONLY,
+		Vertex::FVF,
+		D3DPOOL_MANAGED,
+		&_pVertexBuffer,
+		0)))
+		return E_FAIL;
+
+	// set vertex buffer
+	Vertex* pVertices;
+	_pVertexBuffer->Lock(0, 0, (void**)&pVertices, 0);
+
+	// front face
+	pVertices[0] = Vertex(-1.0f, 1.0f, -1.0f, TD_COLOR_TILE);
+	pVertices[1] = Vertex(1.0f, 1.0f, -1.0f, TD_COLOR_TILE);
+	pVertices[2] = Vertex(-1.0f, -1.0f, -1.0f, TD_COLOR_TILE);
+	pVertices[3] = Vertex(1.0f, -1.0f, -1.0f, TD_COLOR_TILE);
+
+	// back face
+	pVertices[4] = Vertex(-1.0f, 1.0f, 1.0f, TD_COLOR_TILE);
+	pVertices[5] = Vertex(1.0f, 1.0f, 1.0f, TD_COLOR_TILE);
+	pVertices[6] = Vertex(-1.0f, -1.0f, 1.0f, TD_COLOR_TILE);
+	pVertices[7] = Vertex(1.0f, -1.0f, 1.0f, TD_COLOR_TILE);
+
+	_pVertexBuffer->Unlock();
+
+	return S_OK;
+}
+HRESULT TowerDefense::initIndexBuffer()
+{
+	// create index buffer
+	if (FAILED(_pd3dDevice->CreateIndexBuffer(
+		TD_NUM_INDICES * sizeof(WORD),
+		D3DUSAGE_WRITEONLY,
+		D3DFMT_INDEX16,
+		D3DPOOL_MANAGED,
+		&_pIndexBuffer,
+		0)))
+		return E_FAIL;
+
+	// Set indices
+	WORD* pIndices;
+	_pIndexBuffer->Lock(0, 0, (void**)&pIndices, 0);
+
+	// front side
+	pIndices[0] = 0; pIndices[1] = 1; pIndices[2] = 2;
+	pIndices[3] = 2; pIndices[4] = 1; pIndices[5] = 3;
+
+	// back side
+	pIndices[6] = 5; pIndices[7] = 4; pIndices[8] = 7;
+	pIndices[9] = 7; pIndices[10] = 4; pIndices[11] = 6;
+
+	// left side
+	pIndices[12] = 4; pIndices[13] = 0; pIndices[14] = 6;
+	pIndices[15] = 6; pIndices[16] = 0; pIndices[17] = 3;
+
+	// right side
+	pIndices[18] = 1; pIndices[19] = 5; pIndices[20] = 3;
+	pIndices[21] = 3; pIndices[22] = 5; pIndices[23] = 7;
+
+	// top
+	pIndices[24] = 4; pIndices[25] = 5; pIndices[26] = 0;
+	pIndices[27] = 0; pIndices[28] = 5; pIndices[29] = 1;
+
+	// bottom
+	pIndices[30] = 2; pIndices[31] = 3; pIndices[32] = 6;
+	pIndices[33] = 6; pIndices[34] = 3; pIndices[35] = 7;
+
+	_pIndexBuffer->Unlock();
+
+	return S_OK;
+}
+HRESULT TowerDefense::initCamera()
+{
+	// set view space
+	D3DXVECTOR3 cameraPosition = TD_CAMERA_POSITION;
+	D3DXVECTOR3 targetPosition = TD_TARGET_POSITION;
+	D3DXVECTOR3 upVector = TD_WORLD_UP_VECTOR;
+
+	D3DXMATRIX viewMatrix;
+	D3DXMatrixLookAtLH(&viewMatrix, &cameraPosition, &targetPosition, &upVector);
+	_pd3dDevice->SetTransform(D3DTS_VIEW, &viewMatrix);
+
+	// set projection
+	D3DXMATRIX projectionMatrix;
+	D3DXMatrixPerspectiveFovLH(&projectionMatrix,
+		TD_PROJECTION_ANGLE,
+		(float)TD_WINDOW_WIDTH / (float)TD_WINDOW_HEIGHT,
+		TD_PROJECTION_NEAR,
+		TD_PROJECTION_FAR);
+	_pd3dDevice->SetTransform(D3DTS_PROJECTION, &projectionMatrix);
+	_pd3dDevice->SetRenderState(D3DRS_LIGHTING, false);
+
+	return S_OK;
+}
+
+VOID TowerDefense::Finalize() {
+	Cleanup();
+}
---
+++ TowerDefense/TowerDefense/TowerDefense.h
@@ -0,0 +1,78 @@
+#ifndef __TOWER_DEFENSE_H__
+#define __TOWER_DEFENSE_H__
+
+#include <d3d9.h>
+#pragma warning( disable : 4996 ) // disable deprecated warning 
+#include <strsafe.h>
+#pragma warning( default : 4996 )
+#include <d3dx9.h>
+
+//-----------------------------------------------------------------------------
+// Constants
+//-----------------------------------------------------------------------------
+#define TD_BACKGROUND_COLOR		D3DCOLOR_XRGB(255, 255, 255)
+#define TD_WINDOW_TITLE			(L"Tower Defense")
+#define TD_WINDOW_WIDTH			600
+#define TD_WINDOW_HEIGHT		600
+
+#define TD_NUM_VERTICES			8	// cube vertices
+#define TD_NUM_INDICES			36	// cube indices
+
+#define TD_CAMERA_POSITION		D3DXVECTOR3(5.0f, 5.0f, -5.0f)
+#define TD_TARGET_POSITION		D3DXVECTOR3(0.0f, 0.0f,  0.0f)
+#define TD_WORLD_UP_VECTOR		D3DXVECTOR3(0.0f, 1.0f,  0.0f)
+
+#define TD_PROJECTION_ANGLE		D3DX_PI * 0.5f
+#define TD_PROJECTION_NEAR		1.0f
+#define TD_PROJECTION_FAR		1000.0f
+
+#define TD_COLOR_TILE			D3DCOLOR_XRGB(0, 0, 255)
+
+class TowerDefense
+{
+private:
+	static TowerDefense* _pInstance;
+
+	LPDIRECT3D9				_pD3D;			// Used to create the D3DDevice
+	LPDIRECT3DDEVICE9		_pd3dDevice;	// Our rendering device
+	IDirect3DVertexBuffer9* _pVertexBuffer;
+	IDirect3DIndexBuffer9*  _pIndexBuffer;
+
+	struct Vertex {
+		Vertex() {}
+		Vertex(float x, float y, float z, DWORD color) {
+			_x = x, _y = y, _z = z;
+			_color = color;
+		}
+
+		float _x, _y, _z;
+		D3DCOLOR _color; // 32bit color:ARGB
+		static const DWORD FVF;
+	};
+
+
+	VOID init();
+	HRESULT initVertexBuffer();
+	HRESULT initIndexBuffer();
+	HRESULT initCamera();
+	
+public:
+	TowerDefense();
+
+	static TowerDefense* getInstance() {
+		if (_pInstance == nullptr) {
+			_pInstance = new TowerDefense();
+		}
+		return _pInstance;
+	}
+	VOID Finalize();
+
+	HRESULT InitD3D(HWND hWnd);
+	VOID Cleanup();
+	VOID Render();
+
+	HRESULT SetUp();
+};
+
+
+#endif
